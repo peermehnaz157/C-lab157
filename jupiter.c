a743b8ffa5156714b6b06da7847115346091875d
peermehnaz157 <151817365+peermehnaz157@users.noreply.github.com>
2023-11-28 18:01:19 +0530

    Add files via upload

---
+++ jupiter.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main(){
+    char ch;
+    printf("Enter the charcter\n");
+    scanf("%c",&ch);
+    if (ch=='a'){
+        printf("Character is a Vowel");
+    }
+    else if(ch=='e')
+    {
+        printf("Character is a Vowel");
+    }
+    else if(ch=='i'){
+        printf("Character is a Vowel");
+    }
+    else if(ch=='o'){ 
+        printf("Character is a Vowel");
+    }
+    else if(ch=='u'){
+        printf("Character is a Vowel");
+    }
+    else {
+        printf("Character is a Consonant");
+    }
+    return 0;
+}
