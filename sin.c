f00e90763ca77970f2509fd74977b13021c8e8a9
peermehnaz157 <151817365+peermehnaz157@users.noreply.github.com>
2023-11-28 17:53:15 +0530

    Add files via upload

---
+++ sin.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main (){
+    int a,b;
+    printf("Enter the number");
+    scanf("%d",&a);
+    if (a%2==0){
+        printf("%d is even",a);
+    }
+    else{
+        printf("%d is odd",a);
+    }
+    return 0;
+}
